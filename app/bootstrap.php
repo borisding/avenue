07427db660cd7752f177bc3313fabf9e2290a092
borisding <boris.ding82@outlook.com>
2015-11-05 22:28:35 +0800

    file renaming

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -3,11 +3,11 @@ $app = new \Avenue\App();
 
 // include application service file.
 // any new services should be added into this service file.
-require_once AVENUE_APP_DIR. '/service.php';
+require_once AVENUE_APP_DIR. '/services.php';
 
 // include application route(s)
 // any new routes should be added into this route file.
-require_once AVENUE_APP_DIR. '/route.php';
+require_once AVENUE_APP_DIR. '/routes.php';
 
 // rendering application's output
 $app->render();
