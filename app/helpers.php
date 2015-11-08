ef2d314d90fbaf944ec3707af36f233e9643a56e
borisding <boris.ding82@outlook.com>
2015-11-08 10:34:01 +0800

    refactor

--- app/helpers.php
+++ app/helpers.php
@@ -2,12 +2,10 @@
 // Registering custom view helpers
 // Helper(s) can be called in view template.
 
-$view = $app->singleton('view');
-
-$view->register('upper', function($input) {
+$app->view->register('upper', function($input) {
     return strtoupper($input);
 });
 
-$view->register('lower', function($input) {
+$app->view->register('lower', function($input) {
     return strtolower($input);
 });
