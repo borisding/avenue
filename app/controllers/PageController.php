aafcf9e576378afc6e623ba85148b1ce1abd1be2
borisding <boris.ding82@outlook.com>
2015-11-10 23:15:14 +0800

    move page to layouts

--- app/controllers/PageController.php
+++ app/controllers/PageController.php
@@ -29,7 +29,7 @@ class PageController extends Controller
     {
         parent::beforeAction();
         
-        $this->layout = 'layout';
+        $this->layout = 'layouts/page';
     }
     
     /**
