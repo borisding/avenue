46d6deb1d16a676cc62fa9f97246ac165189936e
borisding <boris.ding82@outlook.com>
2015-11-28 16:14:20 +0800

    add interface for Street

--- src/avenue/database/Street.php
+++ src/avenue/database/Street.php
@@ -2,8 +2,9 @@
 namespace Avenue\Database;
 
 use Avenue\Database\PdoAdapter;
+use Avenue\Database\StreetInterface;
 
-class Street extends PdoAdapter
+class Street extends PdoAdapter implements StreetInterface
 {
     /**
      * Base model class constructor.
