bd9a97d08b9e78bcf07251a969ea7eb60b6260e5
borisding <boris.ding82@outlook.com>
2015-11-28 21:41:41 +0800

    example of query

--- app/models/User.php
+++ app/models/User.php
@@ -5,5 +5,11 @@ use Avenue\Model;
 
 class User extends Model
 {
-    
+    public function getAll()
+    {
+        $query = $this->cmd('select * from {user}');
+        $result = $query->fetchAll();
+        
+        return $result;
+    }
 }
