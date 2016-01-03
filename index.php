aaee7da99a8c745cde8c2d6b96a988a634d991f5
borisding <boris.ding82@outlook.com>
2016-01-03 14:33:39 +0800

    added AVENUE_PUBLIC_DIR in entry script

--- index.php
+++ index.php
@@ -5,17 +5,20 @@ define('AVENUE_START_TIME', microtime(true));
 // avenue root directory
 define('AVENUE_ROOT_DIR', __DIR__);
 
-// avenue app directory
+// path to avenue app directory
 define('AVENUE_APP_DIR', AVENUE_ROOT_DIR . '/app');
 
-// avenue src directory
-define('AVENUE_SRC_DIR', AVENUE_ROOT_DIR . '/src');
-
-// avenue config directory
+// path to avenue config directory
 define('AVENUE_CONFIG_DIR', AVENUE_ROOT_DIR . '/config');
 
-// avenue log directory
+// path to avenue src directory
+define('AVENUE_SRC_DIR', AVENUE_ROOT_DIR . '/src');
+
+// path to avenue log directory
 define('AVENUE_LOG_DIR', AVENUE_ROOT_DIR . '/log');
+
+// path to avenue public directory
+define('AVENUE_PUBLIC_DIR', AVENUE_ROOT_DIR . '/public');
 
 // path to 'vendor' directory
 define('AVENUE_VENDOR_DIR', AVENUE_ROOT_DIR . '/vendor');
