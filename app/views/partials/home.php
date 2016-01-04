d7e9ba6c4a7f4712b681dfe40d10055ed022cca8
borisding <boris.ding82@outlook.com>
2016-01-04 21:38:14 +0800

    removed all app related files. make avenue core as individual package dependency

--- app/views/partials/home.php
+++
@@ -1,4 +0,0 @@
-<div class="page-header">
-	<h1>Welcome!</h1>
-</div>
-<p class="lead">Start developing your awesome application powered by <a href="https://github.com/borisding/avenue">Avenue framework</a> today!</p>
