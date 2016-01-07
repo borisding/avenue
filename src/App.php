2aa964f503996e0157a408746c91ab2909ea48d2
borisding <boris.ding82@outlook.com>
2016-01-07 20:46:37 +0800

    removed final

--- src/App.php
+++ src/App.php
@@ -19,80 +19,80 @@ use Avenue\Components\SessionFile;
 use Avenue\Helpers\HelperBundleTrait;
 use Avenue\AppInterface;
 
-final class App implements AppInterface
+class App implements AppInterface
 {
     use HelperBundleTrait;
-    
+
     /**
      * Avenue version.
-     * 
+     *
      * @var string
      */
     const AVENUE_VERSION = '1.0';
-    
+
     /**
      * Request instance.
-     * 
+     *
      * @var object
      */
     public $request;
-    
+
     /**
      * Response instance.
-     * 
+     *
      * @var object
      */
     public $response;
-    
+
     /**
      * View instsance.
-     * 
+     *
      * @var object
      */
     public $view;
-    
+
     /**
      * Route instance;
-     * 
+     *
      * @var object
      */
     public $route;
-    
+
     /**
      * Exception instance.
      *
      * @var object
      */
     public $exc;
-    
+
     /**
      * App instance.
-     * 
+     *
      * @var object
      */
     protected static $app;
-    
+
     /**
      * List of respective services.
-     * 
+     *
      * @var array
      */
     protected static $services = [];
-    
+
     /**
      * List of respective class instances.
-     * 
+     *
      * @var array
      */
     protected static $instances = [];
-    
+
     /**
      * List of respective configurations.
-     * 
+     *
      * @var array
      */
     protected static $config = [];
-    
+
     /**
      * App constructor
      */
@@ -101,14 +101,14 @@ final class App implements AppInterface
         if (empty(static::$app)) {
             static::$app = $this;
         }
-        
+
         $this->setTimezone()->setErrorHandler();
         $this->registry()->factory();
     }
-    
+
     /**
      * Adding route's rule for particular request.
-     * 
+     *
      * @see \Avenue\AppInterface::addRoute()
      */
     public function addRoute()
@@ -116,13 +116,13 @@ final class App implements AppInterface
         if (!$this->route->isFulfilled()) {
             return $this->route->init(func_get_args());
         }
-        
+
         return true;
     }
-    
+
     /**
      * Container that registers specific service for later usage.
-     * 
+     *
      * @see \Avenue\AppInterface::container()
      */
     public function container($name, Closure $callback)
@@ -133,10 +133,10 @@ final class App implements AppInterface
 
         static::$services[$name] = $callback;
     }
-    
+
     /**
      * Resolving registered services via callback.
-     * 
+     *
      * @see \Avenue\Interfaces\AppInterface::resolve()
      */
     public function resolve($name)
@@ -145,15 +145,15 @@ final class App implements AppInterface
             $this->response->setStatus(500);
             throw new \OutOfBoundsException(sprintf('Service [%s] is not registered!', $name));
         }
-        
+
         $resolver = static::$services[$name];
         return $resolver();
     }
-    
+
     /**
      * Making sure only one class instance created at one time.
      * Class instance returned by resolving the registered service.
-     * 
+     *
      * @see \Avenue\Interfaces\AppInterface::singleton()
      */
     public function singleton($name)
@@ -161,17 +161,17 @@ final class App implements AppInterface
         if (!array_key_exists($name, static::$instances)) {
             static::$instances[$name] = $this->resolve($name);
         }
-        
+
         if (!is_object(static::$instances[$name])) {
             return null;
         }
-        
+
         return static::$instances[$name];
     }
-    
+
     /**
      * Rendering the response body output.
-     * 
+     *
      * @see \Avenue\Interfaces\AppInterface::render()
      */
     public function render()
@@ -181,7 +181,7 @@ final class App implements AppInterface
             $this->response->setStatus(404);
             throw new \Exception('Page not found!');
         }
-        
+
         // exit if request is via ajax
         // this is to avoid entire view to be re-rendered
         // ajax output can be printed out using standard echo
@@ -189,17 +189,17 @@ final class App implements AppInterface
         if ($this->request->isAjax()) {
             return;
         }
-        
+
         // print out the response body for normal request
         if (!empty($this->response->getBody())) {
             $this->response->render();
         }
     }
-    
+
     /**
      * Set the error and exception handlers.
      * Error messages are render via error service.
-     * 
+     *
      * @throws \ErrorException
      */
     protected function setErrorHandler()
@@ -208,19 +208,19 @@ final class App implements AppInterface
             $this->exc = $exc;
             return $this->error();
         });
-        
+
         set_error_handler(function($severity, $message, $file, $line) {
             if (!(error_reporting() & $severity)) {
                 return;
             }
-            
+
             $this->response->setStatus(500);
             throw new \ErrorException($message, 0, $severity, $file, $line);
         });
-        
+
         return $this;
     }
-    
+
     /**
      * Add the respective application registries.
      */
@@ -229,64 +229,64 @@ final class App implements AppInterface
         $this->container('request', function() {
             return new Request(static::getInstance());
         });
-        
+
         $this->container('response', function() {
             return new Response(static::getInstance());
         });
-        
+
         $this->container('route', function() {
             return new Route(static::getInstance());
         });
-        
+
         $this->container('view', function() {
             return new View(static::getInstance());
         });
-        
+
         $this->container('log', function() {
             return new Log(static::getInstance());
         });
-        
+
         $this->container('exception', function() {
             return new Exception(static::getInstance(), $this->exc);
         });
-        
+
         $this->container('encryption', function() {
             return new Encryption(static::getInstance());
         });
-        
+
         $this->container('pagination', function() {
             return new Pagination(static::getInstance());
         });
-        
+
         $this->container('validation', function() {
             return new Validation(static::getInstance());
         });
-        
+
         $this->container('cookie', function() {
             return new Cookie(static::getInstance());
         });
-        
+
         $this->container('session', function() {
             $config = $this->getConfig('session');
             $storage = $this->arrGet('storage', $config, 'file');
-            
+
             if ($storage === 'database') {
                 return new Session(new SessionDatabase(), $config);
             }
-            
+
             if ($storage === 'cookie') {
                 return new Session(new SessionCookie(static::getInstance(), $this->cookie()), $config);
             }
-            
+
             return new Session(new SessionFile(static::getInstance()), $config);
         });
-        
+
         return $this;
     }
-    
+
     /**
      * Retrieving config value based on the key.
-     * 
+     *
      * @param mixed $key
      */
     public function getConfig($key)
@@ -294,14 +294,14 @@ final class App implements AppInterface
         if (empty(static::$config)) {
             static::$config = require_once AVENUE_CONFIG_DIR . '/app.php';
         }
-        
+
         if (!array_key_exists($key, static::$config)) {
             static::$config[$key] = null;
         }
-    
+
         return static::$config[$key];
     }
-    
+
     /**
      * Returning avenue version.
      */
@@ -309,7 +309,7 @@ final class App implements AppInterface
     {
         return static::AVENUE_VERSION;
     }
-    
+
     /**
      * Returning app instance.
      */
@@ -317,7 +317,7 @@ final class App implements AppInterface
     {
         return static::$app;
     }
-    
+
     /**
      * Set the application default timezone.
      */
@@ -326,7 +326,7 @@ final class App implements AppInterface
         date_default_timezone_set($this->getConfig('timezone'));
         return $this;
     }
-    
+
     /**
      * Retrieve respective class instances via singleton method.
      */
@@ -337,12 +337,12 @@ final class App implements AppInterface
         $this->route = $this->route();
         $this->view = $this->view();
     }
-    
+
     /**
      * App call magic method.
      * Shortcut of creating instance via singleton,
      * and also the user defined function, if any
-     * 
+     *
      * @param mixed $name
      * @param array $params
      * @throws \LogicException
@@ -353,11 +353,11 @@ final class App implements AppInterface
         if (array_key_exists($name, static::$services)) {
             return $this->singleton($name);
         }
-        
+
         if (is_callable($name)) {
             return call_user_func_array($name, $params);
         }
-        
+
         throw new \LogicException(sprintf('Method [%s] does not exist!', $name));
     }
 }
