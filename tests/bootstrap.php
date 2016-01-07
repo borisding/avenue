181a469b7176c5a3a220681ecbddb935f4eef35c
borisding <boris.ding82@outlook.com>
2016-01-07 20:45:12 +0800

    bootstrap config

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,2 +1,9 @@
 <?php
-require_once dirname(__DIR__) . '/vendor/autoload.php';
+// inlucde vendor autoloader
+$autoloader = require dirname(__DIR__) . '/vendor/autoload.php';
+
+// set tests namespace at runtime
+$autoloader->setPsr4('Avenue\\Tests\\', __DIR__);
+
+// set timezone
+date_default_timezone_set('UTC');
