9bc1faaeec97b1d71e188c7aaa341327907e96ab
borisding <boris.ding82@outlook.com>
2016-01-08 23:53:56 +0800

    added more tests

--- tests/src/AppTest.php
+++ tests/src/AppTest.php
@@ -9,6 +9,7 @@ class AppTest extends \PHPUnit_Framework_TestCase
 
     public function setUp()
     {
+        @session_start();
         $this->app = new App();
     }
 
@@ -87,22 +88,85 @@ class AppTest extends \PHPUnit_Framework_TestCase
 
     public function testSingletonReturnsClassInstance()
     {
-        $this->app->container('request', function() {
-            return new \Avenue\Request($this->app);
-        });
-
-        $instance = $this->app->singleton('request');
-        $this->assertTrue($instance instanceof \Avenue\Request, 'Returned class instance is as expected via singleton.');
+        $request = $this->app->singleton('request');
+        $this->assertTrue($request instanceof \Avenue\Request, 'Returned class instance is as expected via singleton.');
     }
 
     public function testSingletonThroughAppMagicCallMethod()
     {
-        $this->app->container('request', function() {
-            return new \Avenue\Request($this->app);
+        $request = $this->app->request();
+        $this->assertTrue($request instanceof \Avenue\Request, 'Returned class instance is as expected via magic call method.');
+    }
+
+    public function testSingletonRequestClassInstance()
+    {
+        $request = $this->app->request();
+        $this->assertTrue($request instanceof \Avenue\Request, '$request is instance of \Avenue\Request.');
+    }
+
+    public function testSingletonResponseClassInstance()
+    {
+        $response = $this->app->response();
+        $this->assertTrue($response instanceof \Avenue\Response, '$response is instance of \Avenue\Response.');
+    }
+
+    public function testSingletonRouteClassInstance()
+    {
+        $route = $this->app->route();
+        $this->assertTrue($route instanceof \Avenue\Route, '$route is instance of \Avenue\Route.');
+    }
+
+    public function testSingletonViewClassInstance()
+    {
+        $view = $this->app->view();
+        $this->assertTrue($view instanceof \Avenue\View, '$view is instance of \Avenue\View.');
+    }
+
+    public function testSingletonLogClassInstance()
+    {
+        $log = $this->app->log();
+        $this->assertTrue($log instanceof \Avenue\Log, '$log is instance of \Avenue\Log.');
+    }
+
+    public function testSingletonExceptionClassInstance()
+    {
+        $this->app->container('exception', function() {
+            $exc = new \Exception();
+            return new \Avenue\Exception(App::getInstance(), $exc);
         });
 
-        $instance = $this->app->request();
-        $this->assertTrue($instance instanceof \Avenue\Request, 'Returned class instance is as expected via magic call method.');
+        $exception = $this->app->exception();
+        $this->assertTrue($exception instanceof \Avenue\Exception, '$exception is instance of \Avenue\Exception.');
+    }
+
+    public function testSingletonEncryptionClassInstance()
+    {
+        $encryption = $this->app->encryption();
+        $this->assertTrue($encryption instanceof \Avenue\Components\Encryption, '$encryption is instance of \Avenue\Components\Encryption.');
+    }
+
+    public function testSingletonPaginationClassInstance()
+    {
+        $pagination = $this->app->pagination();
+        $this->assertTrue($pagination instanceof \Avenue\Components\Pagination, '$pagination is instance of \Avenue\Components\Pagination.');
+    }
+
+    public function testSingletonValidationClassInstance()
+    {
+        $validation = $this->app->validation();
+        $this->assertTrue($validation instanceof \Avenue\Components\Validation, '$validation is instance of \Avenue\Components\Validation.');
+    }
+
+    public function testSingletonCookieClassInstance()
+    {
+        $cookie = $this->app->cookie();
+        $this->assertTrue($cookie instanceof \Avenue\Components\Cookie, '$cookie is instance of \Avenue\Components\Cookie.');
+    }
+
+    public function testSingletonSessionClassInstance()
+    {
+        $session = $this->app->session();
+        $this->assertTrue($session instanceof \Avenue\Components\Session, '$session is instance of \Avenue\Components\Session.');
     }
 
     /**
@@ -111,5 +175,20 @@ class AppTest extends \PHPUnit_Framework_TestCase
     public function testCallAppNonExistMethodException()
     {
         $this->app->appNonExistMethod();
+    }
+
+    public function testGetConfig()
+    {
+        // using reflection object class for retrieving static properties
+        $ro = new \ReflectionObject($this->app);
+        $sp = $ro->getStaticProperties();
+
+        // using 'version' key for testing
+        $this->assertEquals($this->app->getConfig('version'), $sp['config']['version'], 'Config value should be same.');
+    }
+
+    public function testGetVersion()
+    {
+        $this->assertEquals($this->app->getVersion(), App::AVENUE_VERSION, 'Version should be equal.');
     }
 }
