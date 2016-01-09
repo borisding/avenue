2990339ccd8f70bef77123700be39e1f2ce1d96d
borisding <boris.ding82@outlook.com>
2016-01-10 00:49:02 +0800

    method overwrite fix

--- src/Request.php
+++ src/Request.php
@@ -5,94 +5,94 @@ class Request
 {
     /**
      * Avenue class instance.
-     * 
+     *
      * @var mixed
      */
     protected $app;
-    
+
     /**
      * Http get method.
-     * 
+     *
      * @var mixed
      */
     const GET_METHOD = 'GET';
-    
+
     /**
      * Http post method.
-     * 
+     *
      * @var mixed
      */
     const POST_METHOD = 'POST';
-    
+
     /**
      * Http put method.
-     * 
+     *
      * @var mixed
      */
     const PUT_METHOD = 'PUT';
-    
+
     /**
      * Http delete method.
-     * 
+     *
      * @var mixed
      */
     const DELETE_METHOD = 'DELETE';
-    
+
     /**
      * Http options method.
      *
      * @var mixed
      */
     const OPTIONS_METHOD = 'OPTIONS';
-    
+
     /**
      * Http patch method.
      *
      * @var mixed
      */
     const PATCH_METHOD = 'PATCH';
-    
+
     /**
      * Http head method.
      *
      * @var mixed
      */
     const HEAD_METHOD = 'HEAD';
-    
+
     /**
      * Http trace method.
      *
      * @var mixed
      */
     const TRACE_METHOD = 'TRACE';
-    
+
     /**
      * Http connect method.
      *
      * @var mixed
      */
     const CONNECT_METHOD = 'CONNECT';
-    
+
     /**
      * Request class constructor.
-     * 
+     *
      * @param App $app
      */
     public function __construct(App $app)
     {
         $this->app = $app;
     }
-    
+
     /**
      * Return true if http request method is GET.
-     * 
+     *
      * @return boolean
      */
     public function isGet()
     {
-        return $this->getRequestMethod() === static::GET_METHOD;    
+        return $this->getRequestMethod() === static::GET_METHOD;
     }
-    
+
     /**
      * Return true if http request method is POST.
      *
@@ -102,7 +102,7 @@ class Request
     {
         return $this->getRequestMethod() === static::POST_METHOD;
     }
-    
+
     /**
      * Return true if http request method is PUT.
      *
@@ -112,7 +112,7 @@ class Request
     {
         return $this->getRequestMethod() === static::PUT_METHOD;
     }
-    
+
     /**
      * Return true if http request method is DELETE.
      *
@@ -122,7 +122,7 @@ class Request
     {
         return $this->getRequestMethod() === static::DELETE_METHOD;
     }
-    
+
     /**
      * Return true if http request method is OPTIONS.
      *
@@ -132,7 +132,7 @@ class Request
     {
         return $this->getRequestMethod() === static::OPTIONS_METHOD;
     }
-    
+
     /**
      * Return true if http request method is PATCH.
      *
@@ -142,7 +142,7 @@ class Request
     {
         return $this->getRequestMethod() === static::PATCH_METHOD;
     }
-    
+
     /**
      * Return true if http request method is HEAD.
      *
@@ -152,7 +152,7 @@ class Request
     {
         return $this->getRequestMethod() === static::HEAD_METHOD;
     }
-    
+
     /**
      * Return true if http request method is TRACE.
      *
@@ -162,7 +162,7 @@ class Request
     {
         return $this->getRequestMethod() === static::TRACE_METHOD;
     }
-    
+
     /**
      * Return true if http request method is CONNECT.
      *
@@ -172,29 +172,29 @@ class Request
     {
         return $this->getRequestMethod() === static::CONNECT_METHOD;
     }
-    
+
     /**
      * Return http request method.
-     * Check against 'X-HTTP-Method-Override' and '_method' as well.
-     * If none was found, then using GET as default. 
+     * Check against 'X-Http-Method-Override' and '_method' as well.
+     * If none was found, then using GET as default.
      * If lower case is true, returned as lower case instead.
-     * 
+     *
      * @param string $lowerCase
      * @return mixed
      */
     public function getRequestMethod($lowerCase = false)
     {
-        if ($this->getHeader('X-HTTP-Method-Override')) {
-            $requestMethod = $this->getHeader('X-HTTP-Method-Override');
+        if ($this->getHeader('X-Http-Method-Override')) {
+            $requestMethod = $this->getHeader('X-Http-Method-Override');
         } elseif (isset($_POST['_method']) && !empty($_POST['_method'])) {
             $requestMethod = $_POST['_method'];
         } else {
             $requestMethod = $this->app->arrGet('REQUEST_METHOD', $_SERVER, static::GET_METHOD);
         }
-        
+
         return ($lowerCase) ? strtolower($requestMethod) : $requestMethod;
     }
-    
+
     /**
      * Return true if request is called via Ajax.
      *
@@ -205,10 +205,10 @@ class Request
         if (isset($_SERVER['HTTP_X_REQUESTED_WITH'])) {
             return strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest';
         }
-        
+
         return false;
     }
-    
+
     /**
      * To check if http is in secure mode.
      *
@@ -218,17 +218,17 @@ class Request
     {
         return isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' || $_SERVER['SERVER_PORT'] == 443;
     }
-    
+
     /**
      * Get particular header value based on the name.
-     * 
+     *
      * @param mixed $key
      */
     public function getHeader($key)
     {
         return $this->app->arrGet($key, $this->getAllHeaders());
     }
-    
+
     /**
      * Get all http request headers.
      */
@@ -237,46 +237,46 @@ class Request
         if (!function_exists('getallheaders')) {
             return $this->getCustomAllHeaders();
         }
-        
+
         return getallheaders();
     }
-    
+
     /**
      * Get all http request headers via alternative, eg: nginx
      */
     public function getCustomAllHeaders()
     {
         $headers = [];
-        
+
         if (!is_array($_SERVER)) {
             return $headers;
         }
-        
+
         foreach ($_SERVER as $key => $value) {
-            
+
             if (substr($key, 0, 5) == 'HTTP_') {
                 $key = substr($key, 5);
                 $key = str_replace('_', ' ', $key);
                 $key = ucwords(strtolower($key));
                 $key = str_replace(' ', '-', $key);
-                
+
                 $headers[$key] = $value;
             }
         }
-        
+
         return $headers;
     }
-    
+
     /**
      * Get url path info.
-     * 
+     *
      * @return mixed
      */
     public function getPathInfo()
     {
         return $this->app->arrGet('PATH_INFO', $_SERVER);
     }
-    
+
     /**
      * Get query string parameters.
      *
@@ -286,7 +286,7 @@ class Request
     {
         return $this->app->arrGet('QUERY_STRING', $_SERVER);
     }
-    
+
     /**
      * Get the host name.
      */
@@ -294,17 +294,17 @@ class Request
     {
         return $this->app->arrGet('HTTP_HOST', $_SERVER);
     }
-    
+
     /**
      * Get current URL's scheme.
-     * 
+     *
      * @return string
      */
     public function getScheme()
     {
         return $this->isSecure() ? 'https' : 'http';
     }
-    
+
     /**
      * Get the script name.
      */
@@ -312,7 +312,7 @@ class Request
     {
         return $this->app->arrGet('SCRIPT_NAME', $_SERVER);
     }
-    
+
     /**
      * Get the current request URI.
      */
@@ -320,7 +320,7 @@ class Request
     {
         return $this->app->arrGet('REQUEST_URI', $_SERVER);
     }
-    
+
     /**
      * Get the user agent.
      */
@@ -328,20 +328,20 @@ class Request
     {
         return $this->app->arrGet('HTTP_USER_AGENT', $_SERVER, 'Unknown');
     }
-    
+
     /**
      * Return the base URL.
-     * 
+     *
      * @return string
      */
     public function getBaseUrl()
     {
         $scriptName = $this->getScriptName();
         $appDir = str_replace(basename($scriptName), '', $scriptName);
-        
+
         return sprintf('%s://%s%s', $this->getScheme(), $this->getHost(), $appDir);
     }
-    
+
     /**
      * Redirect to specified path.
      * Default is full path with base url appended.
@@ -354,29 +354,29 @@ class Request
         if ($baseUrl) {
             $path = $this->getBaseUrl() . $path;
         }
-        
+
         $this->app->response->setStatus(302);
         header('Location:' . $path);
-        
+
         die();
     }
-    
+
     /**
      * Return the raw data via request body.
-     * 
+     *
      * @return string
      */
     public function getBody()
     {
         $input = file_get_contents('php://input');
-        
+
         if (empty($input)) {
             $input = '';
         }
-        
+
         return $input;
     }
-    
+
     /**
      * Return requested directory.
      */
@@ -384,7 +384,7 @@ class Request
     {
         return $this->app->route->getParams('@directory');
     }
-    
+
     /**
      * Return requested controller.
      */
@@ -392,7 +392,7 @@ class Request
     {
         return $this->app->route->getParams('@controller');
     }
-    
+
     /**
      * Return requested action.
      */
