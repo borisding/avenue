12cb0455bb80b7480a3703e345feac6227041d5f
borisding <boris.ding82@outlook.com>
2016-01-10 00:14:31 +0800

    added tests for request class

---
+++ tests/src/HttpMockTest.php
@@ -0,0 +1,39 @@
+<?php
+namespace Avenue\Tests;
+
+class HttpMockTest
+{
+    public function __construct()
+    {
+        $_SERVER['SERVER_NAME'] = 'localhost';
+        $_SERVER['SERVER_PORT'] = '8888';
+        $_SERVER['SERVER_PROTOCOL'] = 'HTTP/1.1';
+        $_SERVER['REMOTE_ADDR'] = '127.0.0.1';
+        $_SERVER['SCRIPT_NAME'] = '/index.php';
+        $_SERVER['REQUEST_URI'] = '/';
+        $_SERVER['REQUEST_METHOD'] = 'GET';
+        $_SERVER['HTTP_X_REQUESTED_WITH'] = 'XMLHttpRequest';
+        $_SERVER['HTTPS'] = 'on';
+        $_SERVER['HTTP_USER_AGENT'] = 'Avenue';
+        $_SERVER['HTTP_X_FORWARDED_FOR'] = '32.32.32.32';
+    }
+
+    public function getDefault()
+    {
+        return $_SERVER;
+    }
+
+    public function set($name, $value)
+    {
+        $_SERVER[$name] = $value;
+    }
+
+    public function get($name)
+    {
+        if (isset($_SERVER[$name])) {
+            return $_SERVER[$name];
+        }
+
+        return '';
+    }
+}
---
+++ tests/src/RequestTest.php
@@ -0,0 +1,74 @@
+<?php
+namespace Avenue\Tests;
+
+use Avenue\App;
+use Avenue\Tests\HttpMockTest;
+
+class RequestTest extends \PHPUnit_Framework_TestCase
+{
+    private $app;
+
+    private $request;
+
+    private $http;
+
+    public function setUp()
+    {
+        $this->app = new App();
+        $this->http = new HttpMockTest();
+        $this->request = $this->app->request();
+    }
+
+    public function testIsGet()
+    {
+        $this->assertTrue($this->request->isGet());
+    }
+
+    public function testIsPost()
+    {
+        $this->http->set('REQUEST_METHOD', 'POST');
+        $this->assertTrue($this->request->isPost());
+    }
+
+    public function testIsPut()
+    {
+        $this->http->set('REQUEST_METHOD', 'PUT');
+        $this->assertTrue($this->request->isPut());
+    }
+
+    public function testIsDelete()
+    {
+        $this->http->set('REQUEST_METHOD', 'DELETE');
+        $this->assertTrue($this->request->isDelete());
+    }
+
+    public function testIsOptions()
+    {
+        $this->http->set('REQUEST_METHOD', 'OPTIONS');
+        $this->assertTrue($this->request->isOptions());
+    }
+
+    public function testIsPatch()
+    {
+        $this->http->set('REQUEST_METHOD', 'PATCH');
+        $this->assertTrue($this->request->isPatch());
+    }
+
+    public function testIsHead()
+    {
+        $this->http->set('REQUEST_METHOD', 'HEAD');
+        $this->assertTrue($this->request->isHead());
+    }
+
+    public function testIsTrace()
+    {
+        $this->http->set('REQUEST_METHOD', 'TRACE');
+        $this->assertTrue($this->request->isTrace());
+    }
+
+    public function testIsConnect()
+    {
+        $this->http->set('REQUEST_METHOD', 'CONNECT');
+        $this->assertTrue($this->request->isConnect());
+    }
+}
