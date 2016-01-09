6b12ae305422527baa4fb3b57bbe9491af3e97d1
borisding <boris.ding82@outlook.com>
2016-01-10 00:48:05 +0800

    more tests

--- tests/src/RequestTest.php
+++ tests/src/RequestTest.php
@@ -73,4 +73,70 @@ class RequestTest extends \PHPUnit_Framework_TestCase
         $this->http->set('REQUEST_METHOD', 'CONNECT');
         $this->assertTrue($this->request->isConnect());
     }
+
+    public function testGetRequestMethodGetLowercase()
+    {
+        $this->http->set('REQUEST_METHOD', 'GET');
+        $this->assertEquals('get', $this->request->getRequestMethod(true));
+    }
+
+    public function testGetRequestMethodPostLowercase()
+    {
+        $this->http->set('REQUEST_METHOD', 'POST');
+        $this->assertEquals('post', $this->request->getRequestMethod(true));
+    }
+
+    public function testGetRequestMethodPutLowercase()
+    {
+        $this->http->set('REQUEST_METHOD', 'PUT');
+        $this->assertEquals('put', $this->request->getRequestMethod(true));
+    }
+
+    public function testGetRequestMethodDeleteLowercase()
+    {
+        $this->http->set('REQUEST_METHOD', 'DELETE');
+        $this->assertEquals('delete', $this->request->getRequestMethod(true));
+    }
+
+    public function testGetRequestMethodOptionsLowercase()
+    {
+        $this->http->set('REQUEST_METHOD', 'OPTIONS');
+        $this->assertEquals('options', $this->request->getRequestMethod(true));
+    }
+
+    public function testGetRequestMethodPatchLowercase()
+    {
+        $this->http->set('REQUEST_METHOD', 'PATCH');
+        $this->assertEquals('patch', $this->request->getRequestMethod(true));
+    }
+
+    public function testGetRequestMethodHeadLowercase()
+    {
+        $this->http->set('REQUEST_METHOD', 'HEAD');
+        $this->assertEquals('head', $this->request->getRequestMethod(true));
+    }
+
+    public function testGetRequestMethodTraceLowercase()
+    {
+        $this->http->set('REQUEST_METHOD', 'TRACE');
+        $this->assertEquals('trace', $this->request->getRequestMethod(true));
+    }
+
+    public function testGetRequestMethodConnectLowercase()
+    {
+        $this->http->set('REQUEST_METHOD', 'CONNECT');
+        $this->assertEquals('connect', $this->request->getRequestMethod(true));
+    }
+
+    public function testGetRequestMethodWithHeaderOverride()
+    {
+        $this->http->set('HTTP_X_HTTP_METHOD_OVERRIDE', 'PUT');
+        $this->assertTrue($this->request->isPut());
+    }
+
+    public function testGetRequestMethodWithPostMethod()
+    {
+        $_POST['_method'] = 'PUT';
+        $this->assertTrue($this->request->isPut());
+    }
 }
