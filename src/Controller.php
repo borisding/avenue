ab3e91cc3b48d14307def14b6a391372cd54d5b4
borisding <boris.ding82@outlook.com>
2016-01-11 00:32:26 +0800

    return while calling action

--- src/Controller.php
+++ src/Controller.php
@@ -11,49 +11,49 @@ abstract class Controller
      * @var mixed
      */
     protected $app;
-    
+
     /**
      * Request class instance.
      *
      * @var mixed
      */
     protected $request;
-    
+
     /**
      * Response class instance.
      *
      * @var mixed
      */
     protected $response;
-    
+
     /**
      * Route class instance.
      *
      * @var mixed
      */
     protected $route;
-    
+
     /**
      * View class instance.
      *
      * @var mixed
      */
     protected $view;
-    
+
     /**
      * Magic method parameter.
      *
      * @var array
      */
     protected $params = [];
-    
+
     /**
      * Suffix of controller action.
-     * 
+     *
      * @var string
      */
     const ACTION_SUFFIX = 'Action';
-    
+
     /**
      * Controller class constructor.
      *
@@ -64,7 +64,7 @@ abstract class Controller
         $this->app = $app;
         $this->factory()->invoke();
     }
-    
+
     /**
      * Controller's before action method.
      * Invoked before the controller action is called.
@@ -73,7 +73,7 @@ abstract class Controller
     {
         // do nothing
     }
-    
+
     /**
      * Controller's after action method.
      * Invoke after the controller action is called.
@@ -82,23 +82,23 @@ abstract class Controller
     {
         // do nothing
     }
-    
+
     /**
      * Invoke targeted controller's action.
      */
     protected function controllerAction()
     {
         $action = $this->request->getAction() . static::ACTION_SUFFIX;
-        
+
         // check if controller action does exit before invoking action
         if (!method_exists($this, $action)) {
             $this->response->setStatus(404);
             throw new \BadMethodCallException(sprintf('Controller action method [%s] not found.', $action));
         }
-        
-        call_user_func_array([$this, $action], []);
+
+        return call_user_func_array([$this, $action], []);
     }
-    
+
     /**
      * Create respective class instances for controller usage.
      * Mapped and re-assign to the core instances.
@@ -109,10 +109,10 @@ abstract class Controller
         $this->response = $this->app->response();
         $this->route = $this->app->route();
         $this->view = $this->app->view();
-    
+
         return $this;
     }
-    
+
     /**
      * Invoke respective controller actions in sequence.
      */
@@ -122,7 +122,7 @@ abstract class Controller
         $this->controllerAction();
         $this->afterAction();
     }
-    
+
     /**
      * Set magic method of controller.
      *
@@ -133,7 +133,7 @@ abstract class Controller
     {
         $this->params[$key] = $value;
     }
-    
+
     /**
      * Get magic method of controller.
      *
@@ -144,7 +144,7 @@ abstract class Controller
     {
         return $this->app->arrGet($key, $this->params);
     }
-    
+
     /**
      * Controller index abstract method.
      * This to ensure child controller class has at least index method.
