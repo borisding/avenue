5980bce081e7ecd5fb723a4642dc056df7995771
borisding <boris.ding82@outlook.com>
2016-01-23 14:19:55 +0800

    test case for required

---
+++ tests/src/components/ValidationTest.php
@@ -0,0 +1,68 @@
+<?php
+namespace Avenue\Tests;
+
+use Avenue\App;
+use Avenue\Tests\Reflection;
+
+class ValidationTest extends \PHPUnit_Framework_TestCase
+{
+    private $app;
+
+    private $v;
+
+    public function setUp()
+    {
+        $this->app = new App();
+        $this->v = $this->app->validation();
+    }
+
+    public function testInitWithParams()
+    {
+        $_POST['test'] = 'validation';
+        $this->v->init($_POST);
+
+        $fields = Reflection::getPropertyValue($this->v, 'fields');
+        $this->assertEquals($_POST['test'], $fields['test']);
+    }
+
+    public function testInitWithoutParams()
+    {
+        $_POST['number'] = 123;
+        $this->v->init();
+
+        $fields = Reflection::getPropertyValue($this->v, 'fields');
+        $this->assertEquals($_POST['number'], $fields['number']);
+    }
+
+    public function testIsRequiredFailed()
+    {
+        $_POST['param'] = '';
+        $this->v->init();
+        $this->v->isRequired('param');
+        $this->assertFalse($this->v->hasPassed());
+    }
+
+    public function testIsRequiredPassed()
+    {
+        $_POST['param'] = 'test';
+        $this->v->init();
+        $this->v->isRequired('param');
+        $this->assertTrue($this->v->hasPassed());
+    }
+
+    public function testIsRequiredWithNull()
+    {
+        $_POST['param'] = null;
+        $this->v->init();
+        $this->v->isRequired('param');
+        $this->assertFalse($this->v->hasPassed());
+    }
+
+    public function testIsRequiredWithSpaces()
+    {
+        $_POST['param'] = '     ';
+        $this->v->init();
+        $this->v->isRequired('param');
+        $this->assertFalse($this->v->hasPassed());
+    }
+}
