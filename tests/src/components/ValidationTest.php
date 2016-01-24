ea5956c5ecc6fba31caa03efad6e389fc4b3cfec
borisding <boris.ding82@outlook.com>
2016-01-24 20:50:55 +0800

    more test

--- tests/src/components/ValidationTest.php
+++ tests/src/components/ValidationTest.php
@@ -64,4 +64,106 @@ class ValidationTest extends \PHPUnit_Framework_TestCase
         $this->v->isRequired('param');
         $this->assertFalse($this->v->hasPassed());
     }
+
+    public function testEqualLengthPassed()
+    {
+        $_POST['author'] = 'Boris Ding Poh Hing';
+        $this->v->init();
+        $this->v->equalLength('author', 19);
+        $this->assertTrue($this->v->hasPassed());
+    }
+
+    public function testEqualLengthFailed()
+    {
+        $_POST['author'] = 'Boris Ding Poh Hing';
+        $this->v->init();
+        $this->v->equalLength('author', 20);
+        $this->assertFalse($this->v->hasPassed());
+    }
+
+    public function testMinLengthPassed()
+    {
+        $_POST['input'] = 'abc';
+        $this->v->init();
+        $this->v->minLength('input', 2);
+        $this->assertTrue($this->v->hasPassed());
+    }
+
+    public function testMinLengthFailed()
+    {
+        $_POST['input'] = 'abc';
+        $this->v->init();
+        $this->v->minLength('input', 4);
+        $this->assertFalse($this->v->hasPassed());
+    }
+
+    public function testMaxLengthPassed()
+    {
+        $_POST['input'] = 'abc';
+        $this->v->init();
+        $this->v->maxLength('input', 4);
+        $this->assertTrue($this->v->hasPassed());
+    }
+
+    public function testMaxLengthFailed()
+    {
+        $_POST['input'] = 'abc';
+        $this->v->init();
+        $this->v->maxLength('input', 2);
+        $this->assertFalse($this->v->hasPassed());
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testInRangeLengthInvalidArgumentException()
+    {
+        $_POST['input'] = 'abc';
+        $this->v->init();
+        $this->v->inRangeLength('input', [2, 3, 4]);
+        $this->assertFalse($this->v->hasPassed());
+    }
+
+    public function testInRangeLengthPassed()
+    {
+        $_POST['input'] = 'abc';
+        $this->v->init();
+        $this->v->inRangeLength('input', [2, 3]);
+        $this->assertTrue($this->v->hasPassed());
+    }
+
+    public function testInRangeLengthFailed()
+    {
+        $_POST['input'] = 'abc';
+        $this->v->init();
+        $this->v->inRangeLength('input', [1, 2]);
+        $this->assertFalse($this->v->hasPassed());
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testInRangeInvalidArgumentException()
+    {
+        $_POST['dummy'] = 2;
+        $this->v->init();
+        $this->v->inRange('dummy', [2, 3, 4]);
+        $this->assertFalse($this->v->hasPassed());
+    }
+
+    public function testInRangePassed()
+    {
+        $_POST['dummy'] = 2;
+        $this->v->init();
+        $this->v->inRange('dummy', [2, 4]);
+        $this->assertTrue($this->v->hasPassed());
+    }
+
+    public function testInRangeFailed()
+    {
+        $_POST['dummy'] = 2;
+        $this->v->init();
+        $this->v->inRange('dummy', [2, 4]);
+        $this->assertTrue($this->v->hasPassed());
+    }
 }
