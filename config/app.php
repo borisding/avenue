8babd978bea2ee0c346f12d5d7eca78696130b8f
borisding <boris.ding82@outlook.com>
2016-07-09 22:26:38 +0800

    removed config for components

--- config/app.php
+++ config/app.php
@@ -5,34 +5,22 @@
 return [
     // current application's version
     'version' => '1.0',
-    
+
     // http version that is used
     'http' => '1.1',
-    
+
     // application's timezone
     'timezone' => 'UTC',
-    
+
     // application's environment mode (development, staging, production)
     'environment' => 'development',
-    
+
     // default controller to be assigned when @controller param is empty
     'defaultController' => 'default',
-    
-    // cookie configuration
-    'cookie' => require_once 'cookie.php',
-    
+
     // database configuration
     'database' => require_once 'database.php',
-    
-    // encryption configuration
-    'encryption' => require_once 'encryption.php',
-    
+
     // logging configuration
-    'logging' => require_once 'logging.php',
-    
-    // session configuration
-    'session' => require_once 'session.php',
-    
-    // validation configuration
-    'validation' => require_once 'validation.php'
+    'logging' => require_once 'logging.php'
 ];
