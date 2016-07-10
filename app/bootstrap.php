e61f0475b24772f95cd2eeb8b15af9b9948e68ae
borisding <boris.ding82@outlook.com>
2016-07-10 16:18:52 +0800

    amendments

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -7,11 +7,11 @@ require_once AVENUE_APP_DIR. '/services.php';
 // include error handling.
 require_once AVENUE_APP_DIR. '/errors.php';
 
-// include application view helpers.
-require_once AVENUE_APP_DIR. '/views/helpers.php';
-
 // include application routes
 require_once AVENUE_APP_DIR. '/routes.php';
+
+// include application view helpers.
+require_once AVENUE_APP_DIR. '/views/helpers.php';
 
 // rendering application output
 $app->run();
