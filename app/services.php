bcc25974a119b7a75a03b21fe4a88811384b9c6f
borisding <boris.ding82@outlook.com>
2016-07-12 01:39:12 +0800

    updates

--- app/services.php
+++ app/services.php
@@ -1,16 +1,6 @@
 <?php
 /**
  * File to place other service providers.
- *
- * eg:
- *
- *      $app->container('otherService', function($app) {
- *
- *          // do something awesome here
- *          // could be returning class instance, etc.
- *
- *      });
- *
  */
 
 // settings for Eloquent database service
@@ -18,7 +8,7 @@ $app->container('db', function($app) {
     $environment = $app->getEnvironment();
     $dbConfig = $app->getConfig('database')[$environment];
 
-    $capsule = new Illuminate\Database\Capsule\Manager;
+    $capsule = new \Illuminate\Database\Capsule\Manager;
     $capsule->addConnection($dbConfig);
 
     $capsule->setAsGlobal();
