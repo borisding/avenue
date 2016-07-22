33a6692b176bd6aebf47cb47e79fdf84fcd7de14
borisding <boris.ding82@outlook.com>
2016-07-22 21:58:27 +0800

    use route param

--- app/routes.php
+++ app/routes.php
@@ -5,7 +5,7 @@
 
 $app->container('routes', function($app) {
     // admin dynamic controller route mapping
-    $app->addRoute('/admin(/@controller(/@action(/@id)))', function() {
+    $app->addRoute('/@prefix(/@controller(/@action(/@id)))', function() {
         return [
             '@prefix' => 'admin',
             '@controller' => ':alnum',
