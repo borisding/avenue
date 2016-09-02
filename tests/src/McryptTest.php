b1708977b04d86ed3367238cd4b8b0900ed49d35
borisding <boris.ding82@outlook.com>
2016-09-02 23:59:59 +0800

    refactor

--- tests/src/McryptTest.php
+++ tests/src/McryptTest.php
@@ -1,18 +1,18 @@
 <?php
 use Avenue\App;
 
-use Avenue\Tests\Reflection;
+use Avenue\Mcrypt;
 
 class McryptTest extends \PHPUnit_Framework_TestCase
 {
     private $app;
 
-    private $mcrypt;
+    private $config;
 
     public function setUp()
     {
         $this->app = new App();
-        $this->mcrypt = $this->app->mcrypt();
+        $this->config = ['key' => 'mysecret', 'cipher' => MCRYPT_RIJNDAEL_256, 'mode' => MCRYPT_MODE_CBC];
     }
 
     /**
@@ -20,8 +20,8 @@ class McryptTest extends \PHPUnit_Framework_TestCase
      */
     public function testEmptyKeyException()
     {
-        Reflection::setPropertyValue($this->mcrypt, 'config', ['key' => '', 'cipher' => MCRYPT_RIJNDAEL_256, 'mode' => MCRYPT_MODE_CBC]);
-        $this->mcrypt->encrypt('test');
+        $config = ['key' => '', 'cipher' => MCRYPT_RIJNDAEL_256, 'mode' => MCRYPT_MODE_CBC];
+        $this->getMcrypt($config)->encrypt('test');
     }
 
     /**
@@ -29,8 +29,8 @@ class McryptTest extends \PHPUnit_Framework_TestCase
      */
     public function testCipherNotSupportedException()
     {
-        Reflection::setPropertyValue($this->mcrypt, 'config', ['key' => '', 'cipher' => 'zzz', 'mode' => MCRYPT_MODE_CBC]);
-        $this->mcrypt->encrypt('test');
+        $config = ['key' => '', 'cipher' => 'zzz', 'mode' => MCRYPT_MODE_CBC];
+        $this->getMcrypt($config)->encrypt('test');
     }
 
     /**
@@ -38,33 +38,41 @@ class McryptTest extends \PHPUnit_Framework_TestCase
      */
     public function testModeNotSupportedException()
     {
-        Reflection::setPropertyValue($this->mcrypt, 'config', ['key' => '', 'cipher' => MCRYPT_RIJNDAEL_256, 'mode' => 'zzz']);
-        $this->mcrypt->encrypt('test');
+        $config = ['key' => '', 'cipher' => MCRYPT_RIJNDAEL_256, 'mode' => 'zzz'];
+        $this->getMcrypt($config)->encrypt('test');
     }
 
     public function testEncryptData()
     {
-        Reflection::setPropertyValue($this->mcrypt, 'config', ['key' => 'mysecret', 'cipher' => MCRYPT_RIJNDAEL_256, 'mode' => MCRYPT_MODE_CBC]);
-        $this->assertNotEquals('test', $this->mcrypt->encrypt('test'));
+        $this->assertNotEquals('test', $this->getMcrypt()->encrypt('test'));
     }
 
     public function testDecryptData()
     {
-        Reflection::setPropertyValue($this->mcrypt, 'config', ['key' => 'mysecret', 'cipher' => MCRYPT_RIJNDAEL_256, 'mode' => MCRYPT_MODE_CBC]);
+        $mcrypt = $this->getMcrypt();
         // due to mcrypt_decrypt padding
         // http://www.php.net/manual/en/function.mcrypt-decrypt.php#54734
-        $encryptedData = $this->mcrypt->encrypt('test');
-        $plainText = $this->mcrypt->decrypt($encryptedData);
+        $encryptedData = $mcrypt->encrypt('test');
+        $plainText = $mcrypt->decrypt($encryptedData);
         $this->assertEquals('test', rtrim($plainText, "\0"));
     }
 
     public function testGetListOfSupportedCiphers()
     {
-        $this->assertTrue(is_array($this->mcrypt->getSupportedCiphers()));
+        $this->assertTrue(is_array($this->getMcrypt()->getSupportedCiphers()));
     }
 
     public function testGetListOfSupportedModes()
     {
-        $this->assertTrue(is_array($this->mcrypt->getSupportedModes()));
+        $this->assertTrue(is_array($this->getMcrypt()->getSupportedModes()));
+    }
+
+    private function getMcrypt(array $config = [])
+    {
+        if (empty($config)) {
+            $config = $this->config;
+        }
+
+        return new Mcrypt($this->app, $config);
     }
 }
