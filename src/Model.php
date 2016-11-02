cf49ff3bc89c39d099b1fa20632ecd10e38c7b0c
borisding <boris.ding82@outlook.com>
2016-11-02 22:17:24 +0800

    removed constructor for abstraction

--- src/Model.php
+++ src/Model.php
@@ -5,11 +5,5 @@ use Avenue\Database\Command as BaseModel;
 
 abstract class Model extends BaseModel
 {
-    /**
-     * Model class constructor.
-     */
-    public function __construct()
-    {
-        parent::__construct();
-    }
+    // nothing here
 }
